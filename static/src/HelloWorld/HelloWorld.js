6fab8c2b1151c7ade4669dc0fec63f11631f1977
cyberfolk <andrea1995ferrari@gmail.com>
2024-02-29 00:42:52 +0100

    [ADD] HelloWorld: button toggle, button increment

--- static/src/HelloWorld/HelloWorld.js
+++ static/src/HelloWorld/HelloWorld.js
@@ -3,15 +3,26 @@
 import { Component, useState } from '@odoo/owl';
 
 export class HelloWorld extends Component {
+    static template = 'HelloWorld';
     setup() {
         super.setup();
         this.state = useState({
             name: "Mondo",
+            value: 0,
+            is_primary: true
         });
+    }
+    updateName(newName) {
+        this.state.name = newName;
+    }
+    increment() {
+        this.state.value++;
+    }
+    toggle_is_primary() {
+        this.state.is_primary = this.state.is_primary ? false : true;
     }
 }
 
-HelloWorld.template = 'HelloWorld';
 HelloWorld.components = { HelloWorld };
 
 
